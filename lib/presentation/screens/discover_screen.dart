374f86ff6eea0323bbbe96e3d65b720684001969
Julian Bolaños <julian.bolanos.guerrero@gmail.com>
2023-12-23 15:25:35 -0600

    update

--- lib/presentation/screens/discover_screen.dart
+++ lib/presentation/screens/discover_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/widgets/video_scroll.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Discover Screen')),
+
+    // Referencia a DiscoverProvider para tener acceso a los cambios.
+    //    watch --> permite renderizar widgets si sufren cambios.
+    //    read --> permite solamente escuchar los cambios.
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    return Scaffold(
+      body: discoverProvider.initialLoading
+            ? const Center(child: CircularProgressIndicator(strokeWidth: 2, color: Colors.white,)) // true
+            : VideoScroll(videos: discoverProvider.videos) // false
     );
   }
 }
